93a174652775cb255ad631bcc85c2d263250a69a
=blacklotus08 <=jettorres08@gmail.com>
2024-02-27 19:12:46 +0800

    feat: Created todo library

---
+++ libs/feature/todo/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/feature-todo.module';
+export * from './lib/lib.routes';
---
+++ libs/feature/todo/src/lib/lib.routes.ts
@@ -0,0 +1,5 @@
+import { Route } from '@angular/router';
+
+export const featureTodoRoutes: Route[] = [
+  /* { path: '', pathMatch: 'full', component: InsertYourComponentHere } */
+];
