9dbdc68ecc9df28548e0dfbb556dde0893026f0d
=blacklotus08 <=jettorres08@gmail.com>
2024-02-27 19:59:20 +0800

    feat: Load primeng packages to the app

--- libs/feature/todo/src/lib/feature-todo.module.ts
+++ libs/feature/todo/src/lib/feature-todo.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { featureTodoRoutes } from './lib.routes';
+import { PanelModule } from 'primeng/panel';
+
+
 import { TodoListingComponent } from './components/todo-listing/todo-listing.component';
 import { CreateTodoComponent } from './components/create-todo/create-todo.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(featureTodoRoutes)],
+  imports: [CommonModule, 
+    RouterModule.forChild(featureTodoRoutes),    
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PanelModule
+  ],
   declarations: [TodoListingComponent, CreateTodoComponent],
 })
 export class FeatureTodoModule {}
--- todo/src/app/app.config.ts
+++ todo/src/app/app.config.ts
@@ -1,7 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MessageService } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    importProvidersFrom(
+      HttpClientModule,
+      BrowserAnimationsModule,
+    ),
+    MessageService,
+    DialogService
+  ],
 };
